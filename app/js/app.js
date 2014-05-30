5de4f1170ce4c26438f697fb9c05e4f9824173ad
Star Sail <st4rsail@gmail.com>
2014-05-30 14:04:51 +0100

    Add promise

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
@@ -9,8 +8,36 @@ angular.module('myApp', [
   'myApp.directives',
   'myApp.controllers'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+config(['$routeProvider',
+  function ($routeProvider) {
+    $routeProvider.when('/view1', {
+      templateUrl: 'partials/partial1.html',
+      controller: 'MyCtrl1'
+    });
+    $routeProvider.when('/view2', {
+      templateUrl: 'partials/partial2.html',
+      controller: 'MyCtrl2',
+      resolve: {
+        loadData: loadData,
+        prepData: prepData
+      }
+    });
+    $routeProvider.otherwise({
+      redirectTo: '/view1'
+    });
+  }
+]);
+
+var loadData = function ($q) {
+  var defer = $q.defer();
+  defer.resolve('hello');
+  console.log('1.loadData');
+  return defer.promise;
+};
+
+var prepData = function ($q) {
+  var defer = $q.defer();
+  defer.resolve('world');
+  console.log(angular.uppercase('Hello'));
+  return defer.promise;
+};
